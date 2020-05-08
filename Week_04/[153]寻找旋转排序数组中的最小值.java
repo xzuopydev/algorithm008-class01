0b0d8a3c2b3e9e142bba6edc02ba7dc175a5add1
xzuopydev <xzuosun@gmail.com>
2020-05-08 16:26:33 +0800

    153 寻找旋转排序数组中的最小值

---
+++ Week_04/[153]寻找旋转排序数组中的最小值.java
@@ -0,0 +1,42 @@
+public class Solution {
+
+    /**
+     * 153 寻找旋转排序数组中的最小值
+     * 
+     * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。 (例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2]).
+     * 
+     * 请找出其中最小的元素。 你可以假设数组中不存在重复元素。
+     * 
+     * 示例1： 输入：[3,4,5,1,2] 输出：1
+     * 
+     * 示例2： 输入：[4,5,6,7,0,1,2] 输出：0
+     */
+
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+
+            if (nums[mid] >= nums[left] && nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return nums[left];
+    }
+}
